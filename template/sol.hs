356b443d43bce9d372ac33d096b565978bc4a022
Angus <70203823+ajhawkings@users.noreply.github.com>
2024-12-02 23:39:32 +0000

    Update template

--- template/sol.hs
+++ template/sol.hs
@@ -1,18 +1,18 @@
 import Data.List.Split (splitOn)
 
 -- part 1
-solution1 :: [(Int, Int)] -> Int
+solution1 :: [[Int]] -> Int
 solution1 = undefined
 
 -- part 2
-solution2 :: [(Int, Int)] -> Int
+solution2 :: [[Int]] -> Int
 solution2 = undefined
 
 -- print
 main :: IO ()
 main = do
   content <- readFile "input.txt"
-  let parse line = let [a, b] = splitOn "   " line in (read a, read b)
+  let parse line = map read (splitOn " " line) :: [Int]
   let parsedLines = map parse (lines content)
   print (solution1 parsedLines)
   print (solution2 parsedLines)
